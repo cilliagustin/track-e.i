5e125b5f49e9a260feae703300b089122c521ab0
Agustin cilli <cilli.agustin@gmail.com>
2022-07-19 08:10:50 +0000

    Finish function to toggle between sections + add helper function to hide nav elements and sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,32 @@
+// Variables
 const navLinks = Array.from(document.getElementsByClassName('nav-link'));
+const sections = Array.from(document.getElementsByTagName('section'));
 
+/**
+ * Removes active class from all navbar elements and add class hide
+ * to all sections
+ */
+function blockNavAndSections(){
+    navLinks.forEach(link => {
+        link.classList.remove('active')
+    })
+    sections.forEach(section => {
+        section.classList.add('hide')
+    })
+}
+
+//Toggle between sections and change active class in navbar
 navLinks.forEach(link =>{
     link.addEventListener('click', function changeSection(e){
-        navLinks.forEach(link => {
-            link.classList.remove('active')
+        blockNavAndSections();
+        e.target.classList.add('active');
+        let clickedNav = e.target.textContent.toLowerCase();
+        sections.forEach(section => {
+            if(section.getAttribute('id')=== clickedNav){
+                section.classList.remove('hide')
+            }
         })
-        e.target.classList.add('active')
     })
 })
+
+
