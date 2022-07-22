2013010478944754deac3a4ad33eeb2132309534
Agustin cilli <cilli.agustin@gmail.com>
2022-07-22 17:29:37 +0000

    create a helper function to create all data from inputs

--- assets/js/data-control.js
+++ assets/js/data-control.js
@@ -13,28 +13,32 @@ submit.addEventListener('click', (e)=>{
     } else if (a == null || a == "" || b == null || b == "" || c == null || c == "" || d == null || d == ""){
         alert("Please complete all the fields")} 
     else {
-        //Create an object
-        let obj = {}
-        //Add income/expense property as transaction type to the object
-        if(addSection.classList.contains('income')){
-            obj.transactionType = "income";
-        } else if(addSection.classList.contains('expense')){
-            obj.transactionType = "expense";
-        }
-        //Add the time stamp as a transaction ID
-        obj.transactionId = Date.now();
-        //Add amount, note, date and category to object
-        obj.amount = a;
-        obj.note = b;
-        obj.date = c;
-        obj.categoty = d;
-        //push the object to the data Array
-        data.push(obj)
-        console.log(data)
-        //Clean inputs but mantain current transaction type
-        deleteValues(inputs)
-        inputCategory.classList.remove('active')
-        uncheckRadioInputs(addRadioInput)
-    }
+        createData(a,b,c,d)
+}
 })
+
+function createData(a, b, c, d){
+    //Create an object
+    let obj = {}
+    //Add income/expense property as transaction type to the object
+    if(addSection.classList.contains('income')){
+        obj.transactionType = "income";
+    } else if(addSection.classList.contains('expense')){
+        obj.transactionType = "expense";
+    }
+    //Add the time stamp as a transaction ID
+    obj.transactionId = Date.now();
+    //Add amount, note, date and category to object
+    obj.amount = a;
+    obj.note = b;
+    obj.date = c;
+    obj.categoty = d;
+    //push the object to the data Array
+    data.push(obj)
+    console.log(data)
+    //Clean inputs but mantain current transaction type
+    deleteValues(inputs)
+    inputCategory.classList.remove('active')
+    uncheckRadioInputs(addRadioInput)
+}
 
