6f1517f0b9e7dafbd67da234eb1cbcf8ce9c8819
Agustin cilli <cilli.agustin@gmail.com>
2022-07-24 15:24:12 +0000

    Finish function to populate calendar section

--- assets/js/data-control.js
+++ assets/js/data-control.js
@@ -14,13 +14,17 @@ submit.addEventListener('click', (e)=>{
     } else if (a == null || a == "" || b == null || b == "" || c == null || c == "" || d == null || d == ""){
         alert("Please complete all the fields")} 
     else {
+        //Create data with all transaction information
         createData(a,b,c,d)
+        //Adds to the dataBy date transactions grouped by date and orders ir from most recent to oldestone
         dataByDate = groupBy('date', data)
         console.log(dataByDate)
+        //Populates the calendar section with the dataByDate
         populateCalendar(dataByDate)
 }
 })
 
+//Creates the data with all transactions
 function createData(a, b, c, d){
     //Create an object
     let obj = {}
@@ -46,6 +50,7 @@ function createData(a, b, c, d){
     uncheckRadioInputs(addRadioInput)
 }
 
+//Groups the data by a specific key
 const groupBy = (key,arr) => arr
 .reduce(
     (cache, product) => {
@@ -59,11 +64,20 @@ const groupBy = (key,arr) => arr
     {}
 )
 
+
+//Populate calendar section with dataByDate obj
 function populateCalendar(obj){
+    //Create empty string that will finally populate the calendar section
+    let calendarContent = ""
+    //iterate the object
     for(let transactionDay in obj){
-        let transactionDate = transactionDay;
-        
+        //Create empty string that will include all information of the day
+        let transactionsOfTheDay = "";
+        //Create variable for the result of the day (all the incomes - all expenses)
+        let dailyResult = 0;
+        //Iterate in the objects inside the array for each day
         for(let transaction in obj[transactionDay]){
+            //Get all values for each transaction
             let transactionData = obj[transactionDay][transaction]
             let transactionCategory = transactionData.category
             let transactionNote = transactionData.note
@@ -71,23 +85,55 @@ function populateCalendar(obj){
             let transactionId = transactionData.timeStamp
             let transactionType = transactionData.type
 
+            let transactionAmountStyled
+            //if transaction is income transactionAmountStyled will have a "$"" added and 
+            //this amount will be added to the daily result, otherwise if is expense, transactionAmountStyled
+            //will have "-$" added and the daily reult will have this amount subtracted
+            if(transactionType === "income"){
+                dailyResult = dailyResult + Number(transactionAmount)
+                transactionAmountStyled = `$ ${transactionAmount}`
+            } else if (transactionType === "expense"){
+                dailyResult = dailyResult - Number(transactionAmount)
+                transactionAmountStyled = `-$ ${transactionAmount}`
+            }
+
+            // Add variables to a string with the html code and each loop add it to the transactionsOfTheDay variable
             let transactionLi = `
             <li>
                 <div class="date-movement" data-type="${transactionType}" id="id${transactionId}">
                     <span class="movement-circle"></span>
                     <p class="movement-category">${transactionCategory}</p>
                     <p class="movement-note">${transactionNote}</p>
-                    <p class="movement-amount">$ ${transactionAmount}</p>
+                    <p class="movement-amount">${transactionAmountStyled}</p>
                     <i class="movement-cross fa-solid fa-xmark"></i>
                 </div>
             </li>
-            `
-            console.log(transactionLi)
+            `;
+            transactionsOfTheDay += transactionLi;
         }
-        
-        
-
-
-
+        //if the dailyResult is positive add a "$"", if is negative style it with a "$" between the number and the "-" 
+        if(dailyResult >= 0){
+            dailyResult = `$ ${dailyResult}`
+        } else if(dailyResult < 0){
+            dailyResult = `-$ ${Math.abs(dailyResult)}`
+        }
+        //Create a variable for all the information of the day
+        let dateLi = `
+            <li class="date">
+                    <div class="date-info">
+                        <p>${transactionDay}</p>
+                        <p>${dailyResult}</p>
+                    </div>
+                    <ul>
+            `;
+        //Add all the code with all the transactions and close the ul and li prevoiusly opened
+        dateLi += transactionsOfTheDay;     
+        dateLi += `
+            </ul>
+        </li>
+        `;
+        calendarContent += dateLi;
     }
+    //populate html with all the information
+    calendarList.innerHTML = calendarContent
 }
