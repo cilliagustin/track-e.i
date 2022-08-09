21213370eb9e33be50300b06391556f7fdd9d6cc
Agustin cilli <cilli.agustin@gmail.com>
2022-08-09 17:17:53 +0000

    Add function to create tutorial steps

--- assets/js/tutorial.js
+++ assets/js/tutorial.js
@@ -1,14 +1,17 @@
 const tutorialBtn = document.getElementById("tutorial-button");
+let tutorialIndex
+
+
 
 
 tutorialBtn.addEventListener("click", startTutorial);
 document.body.addEventListener("click", endTutorial)
 
 function startTutorial(){
+    tutorialIndex = 0;
     let tutorialBackground = document.createElement('div');
     tutorialBackground.classList.add('tutorial-background')
     document.body.insertBefore(tutorialBackground, balanceSection)
-
     let modal = document.createElement('div');
     modal.classList.add('modal');
     modal.innerHTML = `
@@ -20,8 +23,16 @@ function startTutorial(){
             <button id="btn-prev"><p>Prev</p><i class="fa-solid fa-circle-chevron-left"></i></button>
             <button id="btn-next"><p>Next</p><i class="fa-solid fa-circle-chevron-right"></i></button>
         </div>`
-
     document.body.insertBefore(modal, balanceSection)
+    tutorialStep()
+}
+
+function tutorialStep(){
+    if(tutorialIndex === 0){
+        let modalButtons = document.querySelectorAll('.modal .modal-body button')
+        centerModal();
+        modalButtons[0].classList.add('hide')
+    }
 }
 
 function endTutorial(e){
@@ -32,4 +43,13 @@ function endTutorial(e){
         tutorialBackground.remove();
         modal.remove();
     }
+}
+
+
+function centerModal(){
+    let modal = document.querySelector(".modal");
+    let width = modal.offsetWidth;
+    let height = modal.offsetHeight;
+    modal.style.left = `calc(50% - ${width / 2}px)`;
+    modal.style.top = `calc(50% - ${height / 2}px)`;
 }
