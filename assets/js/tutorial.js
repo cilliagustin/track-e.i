283290fb5928b8fabaa36b2eb1996d822c2903a5
Agustin cilli <cilli.agustin@gmail.com>
2022-08-10 16:36:08 +0000

    Crate function to change tutorial step when pressing prev next btn

--- assets/js/tutorial.js
+++ assets/js/tutorial.js
@@ -5,7 +5,8 @@ let tutorialIndex
 
 
 tutorialBtn.addEventListener("click", startTutorial);
-document.body.addEventListener("click", endTutorial)
+document.body.addEventListener("click", endTutorial);
+document.body.addEventListener("click", changeTutorialStep);
 
 function startTutorial(){
     tutorialIndex = 0;
@@ -20,11 +21,48 @@ function startTutorial(){
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum id qui ipsa repudiandae est quod dolores aliquid eos dignissimos. Dignissimos?</p>
         </div>
         <div class="modal-body">
-            <button id="btn-prev"><p>Prev</p><i class="fa-solid fa-circle-chevron-left"></i></button>
-            <button id="btn-next"><p>Next</p><i class="fa-solid fa-circle-chevron-right"></i></button>
+            <button data-modal-btn id="btn-prev"><p>Prev</p><i class="fa-solid fa-circle-chevron-left"></i></button>
+            <button data-modal-btn id="btn-next"><p>Next</p><i class="fa-solid fa-circle-chevron-right"></i></button>
         </div>`
     document.body.insertBefore(modal, balanceSection)
     tutorialStep()
+}
+
+function changeTutorialStep(e){
+    //check that a prev next button was pressed
+    if(e.target.hasAttribute('data-modal-btn')){
+        //sum or subtract to index depending on which button was pressed
+        if(e.target.id === "btn-prev"){
+            tutorialIndex--;
+        } else if(e.target.id === "btn-next"){
+            tutorialIndex++;
+        }
+        //hide or show prev next btn
+        if(tutorialIndex === 0){
+            let modalButtons = document.querySelectorAll('.modal .modal-body button');
+            modalButtons[0].classList.add("hide")
+        } else {
+            let modalButtons = document.querySelectorAll('.modal .modal-body button');
+            modalButtons.forEach(btn =>{
+                btn.classList.remove("hide");
+            })
+        }
+
+        //check if there are highlighted elements and delete highlight
+        let prevTutorialElementsArray = document.querySelectorAll('.tutorial-step');
+        prevTutorialElementsArray.forEach(el =>{
+            el.classList.remove("tutorial-step")
+        })
+        
+        //check current index, highlight element with that data tutorial step
+        let tutorialElementsArray = document.querySelectorAll(`[data-tutorial-step="${tutorialIndex}"]`)
+        tutorialElementsArray.forEach(el =>{
+            el.classList.add("tutorial-step")
+        })
+
+        //locate modal next to highlited element
+        locateModal()
+    }
 }
 
 function tutorialStep(){
@@ -32,6 +70,8 @@ function tutorialStep(){
         let modalButtons = document.querySelectorAll('.modal .modal-body button')
         centerModal();
         modalButtons[0].classList.add('hide')
+    } else{
+        console.log("hi")
     }
 }
 
@@ -45,7 +85,6 @@ function endTutorial(e){
     }
 }
 
-
 function centerModal(){
     let modal = document.querySelector(".modal");
     let width = modal.offsetWidth;
@@ -53,3 +92,4 @@ function centerModal(){
     modal.style.left = `calc(50% - ${width / 2}px)`;
     modal.style.top = `calc(50% - ${height / 2}px)`;
 }
+
